3b047c16444030870e15d399af100f31ed313cdf
Andrews Medina <andrewsmedina@gmail.com>
2012-03-07 21:28:46 -0300

    addind django admin

---
+++ project/services/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import Service
+
+
+admin.site.register(Service)
--- project/urls.py
+++ project/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls.defaults import patterns, include, url
 
 from services.api import ServiceResource
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+
+from django.contrib import admin
+admin.autodiscover()
 
 service_resource = ServiceResource()
 
 urlpatterns = patterns('',
     (r'^api/', include(service_resource.urls)),
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
