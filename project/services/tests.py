d367a84c423657d80a9bb0033bba51f5ec39c549
Andrews Medina <andrewsmedina@gmail.com>
2012-03-07 21:24:14 -0300

    service resource using tastypie

--- project/services/tests.py
+++ project/services/tests.py
@@ -1,6 +1,16 @@
 from django.test import TestCase
 
 from .models import Service
+from .api import ServiceResource
+
+
+class SerivceResourceTestCase(TestCase):
+
+    def test_should_use_a_service_queryset(self):
+        self.assertEqual(Service, ServiceResource._meta.queryset.model)
+
+    def test_resource_name_should_be_service(self):
+        self.assertEqual('service', ServiceResource._meta.resource_name)
 
 
 class ServicesModelTestCase(TestCase):
--- project/urls.py
+++ project/urls.py
@@ -1,17 +1,13 @@
 from django.conf.urls.defaults import patterns, include, url
 
+from services.api import ServiceResource
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
+service_resource = ServiceResource()
+
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^project/', include('project.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    (r'^api/', include(service_resource.urls)),
     # url(r'^admin/', include(admin.site.urls)),
 )
